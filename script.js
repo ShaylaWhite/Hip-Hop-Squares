d2e87f6e4b9e3e20f18ce7f889e380d09b8707ba
Shayla White <shayladwhite1@gmail.com>
2023-08-17 08:38:49 -0700

    planned out js phases, completed phase 1 DOM elemnets

---
+++ script.js
@@ -0,0 +1,26 @@
+//Phase #1 of JS Declare variables/ constants
+// Get references to DOM elements
+const startButton = document.getElementById("start-button");
+const replayButton = document.getElementById("replay-button");
+const player1Avatar = document.getElementById("player1-avatar");
+const player2Avatar = document.getElementById("player2-avatar");
+const squares = document.querySelectorAll(".square");
+const hallOfFameTable = document.querySelector(".hall-of-fame-table");
+
+
+
+
+// Phase #2 Define functions
+
+
+
+
+// Phase #3 Add event listerns to the functions thats being called on the DOM elements in Phase #1 and Phase #2
+
+
+
+
+
+
+
+// Call the game function to start the game
