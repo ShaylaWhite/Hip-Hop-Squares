9c2d4b9458071545cd5070e064f11b2177762f46
Shayla White <shayladwhite1@gmail.com>
2023-08-17 09:10:25 -0700

    updated html with all player options

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-//Phase #1 of JS Declare variables/ constants
-// Get references to DOM elements
+//PHASE NUMBER 1 >> Declare variables/ constants/ imports
+
+
+// >>> Import and Initialize Emoji API:
+// >>> Import the emoji-api library and create an instance of the EmojiApi class.
+import emojiApi from 'emoji-api';
+const api = new emojiApi.EmojiApi();
+
+
+// >>> Get references to DOM elements
 const startButton = document.getElementById("start-button");
 const replayButton = document.getElementById("replay-button");
 const player1Avatar = document.getElementById("player1-avatar");
 const player2Avatar = document.getElementById("player2-avatar");
 const squares = document.querySelectorAll(".square");
 const hallOfFameTable = document.querySelector(".hall-of-fame-table");
+
+// >>> Initialize variables
+
 
 
 
