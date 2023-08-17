6bca8312cfb1c67240402e97b0fe06f9fb7cb639
Shayla White <shayladwhite1@gmail.com>
2023-08-17 11:59:09 -0700

    dropped the api and used unicode insted

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 //PHASE NUMBER 1 >> Declare variables/ constants/ imports
 
-    // >>> Import and Initialize Emoji API:
-    // >>> Import the emoji-api library and create an instance of the EmojiApi class.
-import emojiApi from 'emoji-api';
-const api = new emojiApi.EmojiApi();
-
-
     // >>> Get references to DOM elements
 const startButton = document.getElementById("start-button");
 const replayButton = document.getElementById("replay-button");
@@ -25,68 +19,66 @@ let totalMoves = 0; // No moves have been made yet
 
     // >>> Emojis for players imported from the api
     // >>>> Map rapper names to their corresponding emoji names
-const rapperEmojiNames = {
-    drake: 'owl',
-    kendrick: 'black_ninja',
-    cardi: 'moneybag',
-    jayz: 'goat',
-    nipsey: 'checkered_flag',
-    megan: 'horse',
-    jeezy: 'snowflake',
-    dolph: 'dolphin',
-    snoop: 'weed'
-  };
-
-    // >>> Initialize player objects with default values and the logic to choode rapper and match it to its corresponding emoji 
-let player1 = {
-    name: "Player 1",
-    wins: 0,
-    emoji: "" // Emoji will be assigned based on dropdown selection
-  };
+    const rapperEmojis = {
+        drake: '\u{1F989}',     // 🦉
+        kendrick: '\u{1F977}',  // 🥷
+        cardi: '\u{1F4B0}',     // 💰
+        jayz: '\u{1F410}',      // 🐐
+        nipsey: '\u{1F3C1}',    // 🏁
+        megan: '\u{1F40E}',     // 🐎
+        jeezy: '\u{2744}',      // ❄️
+        dolph: '\u{1F42C}',     // 🐬
+        snoop: '\u{1F33F}'      // 🌿
+    };
+    
+    // Initialize player objects with default values and the logic to choose rapper and match it to its corresponding emoji
+    let player1 = {
+        name: "Player 1",
+        wins: 0,
+        emoji: "" // Emoji will be assigned based on dropdown selection
+    };
+    
+    let player2 = {
+        name: "Player 2",
+        wins: 0,
+        emoji: "" // Emoji will be assigned based on dropdown selection
+    };
   
-  let player2 = {
-    name: "Player 2",
-    wins: 0,
-    emoji: "" // Emoji will be assigned based on dropdown selection
-  };
+// Function to initialize player avatar dropdowns with options
+function initializeAvatarDropdowns() {
+    for (const rapper in rapperEmojis) {
+      // Create a new option element
+      const option1 = document.createElement("option");
+      const option2 = document.createElement("option");
   
-        // >>> Event listener for the player avatar dropdowns
-  player1AvatarDropdown.addEventListener("change", updatePlayerAvatars);
-  player2AvatarDropdown.addEventListener("change", updatePlayerAvatars);
+      // Set the value attribute to the rapper name
+      option1.value = rapper;
+      option2.value = rapper;
   
-  async function updatePlayerAvatars() {
-    const selectedRapper1 = player1AvatarDropdown.value;
-    const selectedRapper2 = player2AvatarDropdown.value;
+      // Set the inner text of the option to include the rapper name and emoji
+      option1.innerText = `${rapper} ${rapperEmojis[rapper]}`;
+      option2.innerText = `${rapper} ${rapperEmojis[rapper]}`;
   
-    const emoji1 = await getRapperEmoji(selectedRapper1);
-    const emoji2 = await getRapperEmoji(selectedRapper2);
-  
-    player1.emoji = emoji1;
-    player2.emoji = emoji2;
-  
-    // Update the emojis displayed in the dropdown options
-    player1AvatarDropdown.options[selectedRapper1].innerText = `${selectedRapper1} ${emoji1}`;
-    player2AvatarDropdown.options[selectedRapper2].innerText = `${selectedRapper2} ${emoji2}`;
-  }
-  
-  // Function to get the emoji for a rapper
-  async function getRapperEmoji(rapperName) {
-    try {
-      const emojiName = rapperEmojiNames[rapperName];
-      const response = await api.search(emojiName);
-      const emoji = response[0]?.character || '';
-      return emoji;
-    } catch (error) {
-      console.error("Error fetching rapper emoji:", error);
-      return '';
+      // Append the options to the dropdowns
+      player1AvatarDropdown.appendChild(option1);
+      player2AvatarDropdown.appendChild(option2);
     }
   }
+  
+  // Call the function to initialize the dropdowns
+  initializeAvatarDropdowns();
+  
+  
+  
+ 
+  
+  
 
  // >>> Initialize gamebaord and start of game 
 
 
 
- 
+
 // Phase #3 Add event listerns to the functions thats being called on the DOM elements in Phase #1 and Phase #2
 
 
