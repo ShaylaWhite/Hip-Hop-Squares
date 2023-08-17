461c108bb695893a0cc8518bde4053fa7097864c
Shayla White <shayladwhite1@gmail.com>
2023-08-17 10:53:33 -0700

    wrote out remining phases

--- script.js
+++ script.js
@@ -54,15 +54,34 @@ let player1 = {
   player1AvatarDropdown.addEventListener("change", updatePlayerAvatars);
   player2AvatarDropdown.addEventListener("change", updatePlayerAvatars);
   
-  // Function to update player avatars based on dropdown selection
-  function updatePlayerAvatars() {
+  async function updatePlayerAvatars() {
     const selectedRapper1 = player1AvatarDropdown.value;
     const selectedRapper2 = player2AvatarDropdown.value;
   
-    player1.emoji = rapperEmojiNames[selectedRapper1];
-    player2.emoji = rapperEmojiNames[selectedRapper2];
+    const emoji1 = await getRapperEmoji(selectedRapper1);
+    const emoji2 = await getRapperEmoji(selectedRapper2);
+  
+    player1.emoji = emoji1;
+    player2.emoji = emoji2;
+  
+    // Update the emojis displayed in the dropdown options
+    player1AvatarDropdown.options[selectedRapper1].innerText = `${selectedRapper1} ${emoji1}`;
+    player2AvatarDropdown.options[selectedRapper2].innerText = `${selectedRapper2} ${emoji2}`;
   }
-
+  
+  // Function to get the emoji for a rapper
+  async function getRapperEmoji(rapperName) {
+    try {
+      const emojiName = rapperEmojiNames[rapperName];
+      const response = await api.search(emojiName);
+      const emoji = response[0]?.character || '';
+      return emoji;
+    } catch (error) {
+      console.error("Error fetching rapper emoji:", error);
+      return '';
+    }
+  }
+ // >>> Initialize gamebaord and start of game 
 
 
 // Phase #3 Add event listerns to the functions thats being called on the DOM elements in Phase #1 and Phase #2
