b085b563a300b600db5b4218b4f3384e9995711f
Shayla White <shayladwhite1@gmail.com>
2023-08-17 18:40:55 -0700

    phase 1 and css completed

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-//PHASE NUMBER 1 >> Declare variables/ constants/ imports
-
-    // >>> Get references to DOM elements
+//PHASE NUMBER 1 
+//>> Declare variables/ constants/ imports
+// Get references to DOM elements
 const startButton = document.getElementById("start-button");
 const replayButton = document.getElementById("replay-button");
 const player1AvatarDropdown = document.getElementById("player1-avatar");
 const player2AvatarDropdown = document.getElementById("player2-avatar");
 const squares = document.querySelectorAll(".square");
-const hallOfFameTable = document.querySelector(".hall-of-fame-table");
+const currentWinnerName = document.getElementById("current-winner-name");
+const player1Emoji = document.querySelector(".player1-emoji");
+const player2Emoji = document.querySelector(".player2-emoji");
+const player1Wins = document.querySelector(".player1-wins");
+const player2Wins = document.querySelector(".player2-wins");
 
-
-    // >>> Initialize variables
+// Initialize variables
 let currentPlayer = "player1"; // Player 1 starts first
 let gameBoard = Array(9).fill(""); // Initialize an empty game board
 let winner = null; // No winner at the start
 let isGameOver = false; // The game is not over initially
 let totalMoves = 0; // No moves have been made yet
 
-
-    // >>> Emojis for players imported from the api
-    // >>>> Map rapper names to their corresponding emoji names
-    const rapperEmojis = {
+ 
+// >>>> Map rapper names to their corresponding emoji names
+const rapperEmojis = {
         Drake: '\u{1F989}',     // 🦉
         Kendrick: '\u{1F977}',  // 🥷
         CardiB: '\u{1F4B0}',     // 💰
@@ -31,18 +33,18 @@ let totalMoves = 0; // No moves have been made yet
         SnoopDogg: '\u{1F33F}'      // 🌿
     };
     
-    // Initialize player objects with default values and the logic to choose rapper and match it to its corresponding emoji
-    let player1 = {
-        name: "Player 1",
-        wins: 0,
-        emoji: "" // Emoji will be assigned based on dropdown selection
-    };
-    
-    let player2 = {
-        name: "Player 2",
-        wins: 0,
-        emoji: "" // Emoji will be assigned based on dropdown selection
-    };
+// Initialize player objects with default values and the logic to choose rapper and match it to its corresponding emoji
+let player1 = {
+    name: "Player 1",
+    wins: 0,
+    emoji: "" // Emoji will be assigned based on dropdown selection
+  };
+  
+  let player2 = {
+    name: "Player 2",
+    wins: 0,
+    emoji: "" // Emoji will be assigned based on dropdown selection
+  };
   
 // Function to initialize player avatar dropdowns with options
 function initializeAvatarDropdowns() {
@@ -68,15 +70,13 @@ function initializeAvatarDropdowns() {
   // Call the function to initialize the dropdowns
   initializeAvatarDropdowns();
   
+/////////////////////PHASE NUMBER 2>>>>>>>>>>>>>>>>>>>>//////////
   
-  
-
- // >>> Initialize gamebaord and start of game 
 
 
 
 
-// Phase #3 Add event listerns to the functions thats being called on the DOM elements in Phase #1 and Phase #2
+/////////////////////PHASE NUMBER 3>>>>>>>>>>>>>>>>>>>>//////////
 
 
 
@@ -84,4 +84,6 @@ function initializeAvatarDropdowns() {
 
 
 
-// Call the game function to start the game
+
+
+
